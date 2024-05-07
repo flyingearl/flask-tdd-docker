a709dbf11b7a534884b4499a9a1d8c676c0b745e
Edward Burton <flyingearl@gmail.com>
2024-05-07 11:34:45 +0100

    finished postgres setup

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
-from src import app
+from src import app, db
 from flask.cli import FlaskGroup
 
 
 cli = FlaskGroup(app)
+
+
+@cli.command("recreate_db")
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 
 if __name__ == "__main__":
--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_restx import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -9,6 +10,20 @@ api = Api(app)
 
 app_settings = os.getenv("APP_SETTINGS")
 app.config.from_object(app_settings)
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class Ping(Resource):
